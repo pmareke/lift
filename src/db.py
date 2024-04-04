1a37f38e0f10b4e40578681ae09d9e8ed8629a71
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-04-04 20:57:41 +0200

    feat: refactor db file

--- src/db.py
+++ src/db.py
@@ -1,27 +1,21 @@
 import pymysql.cursors
 
-from pymysql import Connection
+from pymysql.connections import Connection
+from pymysql.cursors import Cursor
+
+
+class PyMySQLCursorWrapper(Cursor):
+    """
+    The pymysql.cursors.Cursor class very nearly works the same as the odbc equivalent. Unfortunately it doesn't
+    understand the '?' in a SQL statement as an argument placeholder, and instead uses '%s'. This wrapper fixes that.
+    """
+
+    def mogrify(self, query: str, args: object = ...) -> str:
+        query = query.replace("?", "%s")
+        return str(super().mogrify(query, args))
 
 
 def create_lift_pass_db_connection(connection_options: dict) -> Connection:
-    try:
-        connection = try_to_connect_with_pymysql(connection_options)
-        return connection
-    except Exception as ex:
-        print(f"unable to connect to db with {ex}")
-
-
-def try_to_connect_with_pymysql(connection_options: dict) -> Connection:
-    class PyMySQLCursorWrapper(pymysql.cursors.Cursor):
-        """
-        The pymysql.cursors.Cursor class very nearly works the same as the odbc equivalent. Unfortunately it doesn't
-        understand the '?' in a SQL statement as an argument placeholder, and instead uses '%s'. This wrapper fixes that.
-        """
-
-        def mogrify(self, query: str, args: object = ...) -> str:
-            query = query.replace("?", "%s")
-            return str(super().mogrify(query, args))
-
     return pymysql.connect(
         host=connection_options["host"],
         user=connection_options["user"],
