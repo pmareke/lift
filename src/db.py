08e7a866e19d81ef86f2cd246d80585d845ef46d
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-04-05 14:15:35 +0200

    feat: document db file

--- src/db.py
+++ src/db.py
@@ -6,8 +6,10 @@ from pymysql.cursors import Cursor
 
 class PyMySQLCursorWrapper(Cursor):
     """
-    The pymysql.cursors.Cursor class very nearly works the same as the odbc equivalent. Unfortunately it doesn't
-    understand the '?' in a SQL statement as an argument placeholder, and instead uses '%s'. This wrapper fixes that.
+    The Cursor class doesn't understand the '?' in a SQL statement as an
+    argument placeholder, and instead uses '%s'.
+
+    This wrapper fixes that.
     """
 
     def mogrify(self, query: str, args: object = ...) -> str:
