1179b48dac7c3689ec079b197c1c6e3a522039bf
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-04-08 19:30:19 +0200

    feat: refactor tests

--- test/acceptance/test_lift_pass.py
+++ test/acceptance/test_lift_pass.py
@@ -10,67 +10,47 @@ class TestLiftPass:
         self.client = app.test_client()
 
     def test_add_new_price(self) -> None:
-        response = self.client.put("/prices", query_string={"type": "test", "cost": 35})
+        query_string = {"type": "test", "cost": 35}
+        response = self.client.put("/prices", query_string=query_string)
 
         expect(response.status_code).to(equal(OK))
 
     @pytest.mark.parametrize("type", ["1jour", "night"])
     def test_people_under_6_does_not_pay(self, type: str) -> None:
-        response = self.client.get("/prices", query_string={"type": type, "age": 5})
+        query_string = {"type": type, "age": 5}
+        response = self.client.get("/prices", query_string=query_string)
 
         expect(response.json).to(equal({"cost": 0}))
 
     def test_is_holiday(self) -> None:
-        response = self.client.get(
-            "/prices", query_string={"type": "1jour", "date": "2019-02-25"}
-        )
+        query_string = {"type": "1jour", "date": "2019-02-25"}
+        response = self.client.get("/prices", query_string=query_string)
 
         expect(response.json).to(equal({"cost": 35}))
 
     def test_is_not_holiday(self) -> None:
-        response = self.client.get(
-            "/prices", query_string={"type": "1jour", "date": "2024-02-26"}
-        )
+        query_string = {"type": "1jour", "date": "2024-02-26"}
+        response = self.client.get("/prices", query_string=query_string)
 
         expect(response.json).to(equal({"cost": 23}))
 
-    @pytest.mark.parametrize(
-        "type,cost",
-        [
-            ("1jour", 35),
-            ("night", 0),
-        ],
-    )
+    @pytest.mark.parametrize("type,cost", [("1jour", 35), ("night", 0)])
     def test_pass_without_age(self, type: str, cost: int) -> None:
-        response = self.client.get("/prices", query_string={"type": type})
+        query_string = {"type": type}
+        response = self.client.get("/prices", query_string=query_string)
 
         expect(response.json).to(equal({"cost": cost}))
 
-    @pytest.mark.parametrize(
-        "age,expected_cost",
-        [
-            (10, 25),
-            (65, 27),
-            (50, 35),
-        ],
-    )
+    @pytest.mark.parametrize("age,expected_cost", [(10, 25), (65, 27), (50, 35)])
     def test_1jour_pass_with_age(self, age: int, expected_cost: int) -> None:
-        response = self.client.get(
-            "/prices", query_string={"type": "1jour", "age": age}
-        )
+        query_string = {"type": "1jour", "age": age}
+        response = self.client.get("/prices", query_string=query_string)
 
         expect(response.json).to(equal({"cost": expected_cost}))
 
-    @pytest.mark.parametrize(
-        "age,expected_cost",
-        [
-            (10, 19),
-            (65, 8),
-        ],
-    )
+    @pytest.mark.parametrize("age,expected_cost", [(10, 19), (65, 8)])
     def test_night_pass_with_age(self, age: int, expected_cost: int) -> None:
-        response = self.client.get(
-            "/prices", query_string={"type": "night", "age": age}
-        )
+        query_string = {"type": "night", "age": age}
+        response = self.client.get("/prices", query_string=query_string)
 
         expect(response.json).to(equal({"cost": expected_cost}))
