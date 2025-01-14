d9aa181c8132b3077f278652efde70350a665b01
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-01-14 09:45:45 +0100

    feat: refactor get_price_controller

--- src/delivery/api/get_price_controller.py
+++ src/delivery/api/get_price_controller.py
@@ -13,11 +13,12 @@ class GetPriceController:
     def get_price(self) -> Response:
         try:
             pass_type = LiftPassType(request.args["type"])
+            age = request.args.get("age")
+            date = request.args.get("date")
+            query = GetPriceQuery(pass_type, age, date)
         except ValueError:
             return make_response({"cost": 0}, OK)
 
-        age = request.args.get("age")
-        date = request.args.get("date")
-        query = GetPriceQuery(pass_type, age, date)
         cost = self.query_handler.execute(query)
+
         return make_response({"cost": cost}, OK)
